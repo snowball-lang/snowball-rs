f62c1599ac3473b2788774937ca0edce5597914c
Mauro Baladés <mauro.balades@tutanota.com>
2024-02-13 19:21:33 +0000

    Added subcommands for cli

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,48 @@ mod reports;
 mod utils;
 
 #[derive(ClapParser)]
+#[clap(name = "Snowball Compiler", version = "0.1.0", author = "Snowball")]
 struct Args {
-    input: String,
+    // we can have "run" or "build" as the subcommand
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(ClapParser)]
+enum SubCommand {
+    Run(Run),
+    Build(Build),
+}
+
+#[derive(ClapParser)]
+struct Run {
+    #[clap(short, long, default_value = "./")]
+    path: String,
+}
+
+#[derive(ClapParser)]
+struct Build {
+    #[clap(short, long, default_value = "./")]
+    path: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let compiler = crate::compiler::Compiler::new(args.input);
+    match args.subcmd {
+        SubCommand::Run(run) => run_command(run),
+        SubCommand::Build(build) => build_command(build),
+    }
+}
+
+fn run_command(run: Run) {
+    let compiler = compiler::Compiler::new(run.path);
+    compiler.run();
+    // TODO: Run the code
+}
+
+fn build_command(build: Build) {
+    let compiler = compiler::Compiler::new(build.path);
     compiler.run();
 }
+
